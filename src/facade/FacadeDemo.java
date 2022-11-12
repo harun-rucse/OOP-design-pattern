c9913eaa47e35fcd87d76bb908d4c16d462d4c83
Harun Or Rashid <s1810776113@ru.ac.bd>
2022-11-12 23:03:28 +0600

    Facade pattern

---
+++ src/facade/FacadeDemo.java
@@ -0,0 +1,11 @@
+package facade;
+
+public class FacadeDemo {
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
---
+++ src/facade/ShapeMaker.java
@@ -0,0 +1,25 @@
+package facade;
+
+public class ShapeMaker {
+    private Circle circle;
+    private Rectangle rectangle;
+    private Square square;
+
+    public ShapeMaker() {
+        circle = new Circle();
+        rectangle = new Rectangle();
+        square = new Square();
+    }
+
+    public void drawCircle() {
+        circle.draw();
+    }
+
+    public void drawRectangle() {
+        rectangle.draw();
+    }
+
+    public void drawSquare() {
+        square.draw();
+    }
+}
