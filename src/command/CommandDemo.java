fe011af30699e6cec586a1328e760c7e388c70ad
Harun Or Rashid <s1810776113@ru.ac.bd>
2022-11-13 22:57:50 +0600

    Command pattern

---
+++ src/command/CommandDemo.java
@@ -0,0 +1,16 @@
+package command;
+
+public class CommandDemo {
+    public static void main(String[] args) {
+        Stock stock = new Stock();
+
+        BuyStock buyStockOrder = new BuyStock(stock);
+        SellStock sellStockOrder = new SellStock(stock);
+
+        Broker broker = new Broker();
+        broker.takeOrder(buyStockOrder);
+        broker.takeOrder(sellStockOrder);
+
+        broker.placeOrder();
+    }
+}
